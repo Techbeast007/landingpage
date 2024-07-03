9eb16bc257912cf20ee98f8f39978f02291896ec
Techbeast007 <navneetpandey34@gmail.com>
2024-07-03 16:28:13 +0530

    changes

--- landingpage.js
+++ landingpage.js
@@ -6,4 +6,37 @@ faqs.forEach((faq) => {
   });
 });
 
+
+    // Set the date we're counting down to
+    var countDownDate = new Date("Aug 22, 2024 00:00:00").getTime();
+
+    // Update the countdown every 1 second
+    var countdownFunction = setInterval(function() {
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the countdown date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes, and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the respective elements
+        document.getElementById("days").innerText = days.toString().padStart(2, '0');
+        document.getElementById("hours").innerText = hours.toString().padStart(2, '0');
+        document.getElementById("minutes").innerText = minutes.toString().padStart(2, '0');
+        document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0');
+
+        // If the countdown is finished, display some text
+        if (distance < 0) {
+            clearInterval(countdownFunction);
+            document.querySelector(".countdown-container").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+
+
+
   
